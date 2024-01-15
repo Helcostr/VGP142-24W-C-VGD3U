7b0d377c680a57d596cecf348b3af89d23bb05cf
Helcostr <15018549+Helcostr@users.noreply.github.com>
2024-01-15 15:15:34 -0500

    - Add CharacterController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private float speed = 1f;
+    [SerializeField]
+    private float gravity = 9.81f;
+    [SerializeField]
+    private float jumpSpeed = 10.0f;
+
+    private CharacterController cc;
+
     void Start() {
-        
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetButton("Cancel"))
             GameManager.instance.Cancel();
+        MoveInput();
+    }
+    private void MoveInput() {
+        float hInput = Input.GetAxis("Horizontal");
+        float fInput = Input.GetAxis("Vertical");
+        Vector3 moveInput = new Vector3(hInput, 0, fInput).normalized;
+
+        moveInput.y = cc.velocity.y;
+        moveInput.y -= gravity * Time.deltaTime;
+        moveInput *= Time.deltaTime;
+
+        cc.Move(moveInput);
     }
 }
