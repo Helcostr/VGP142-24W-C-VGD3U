a9dfff9b021698b0b3becdba1da2c28d023ca227
Helcostr <15018549+Helcostr@users.noreply.github.com>
2024-01-22 11:15:27 -0500

    finish up player ctrl

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -7,12 +8,16 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private float gravity = 9.81f;
     [SerializeField]
-    private float jumpSpeed = 10.0f;
+    private float jumpStrength = 1;
+    [SerializeField, ReadOnly]
+    private bool touching;
 
     private CharacterController cc;
+    private float verticalVelocity;
 
     void Start() {
         cc = GetComponent<CharacterController>();
+        Debug.Log("Scale:" + Time.timeScale);
     }
 
     // Update is called once per frame
@@ -22,14 +27,23 @@ public class PlayerController : MonoBehaviour {
         MoveInput();
     }
     private void MoveInput() {
+        touching = cc.isGrounded;
+        if (cc.isGrounded) {
+            verticalVelocity = -0.5f;
+            if (Input.GetButtonDown("Jump")) {
+                verticalVelocity = jumpStrength;
+            }
+        } else verticalVelocity += -gravity * Time.deltaTime;
+
         float hInput = Input.GetAxis("Horizontal");
         float fInput = Input.GetAxis("Vertical");
-        Vector3 moveInput = new Vector3(hInput, 0, fInput).normalized;
 
-        moveInput.y = cc.velocity.y;
-        moveInput.y -= gravity * Time.deltaTime;
-        moveInput *= Time.deltaTime;
-
+        // Move Vector
+        Vector3 moveInput = new Vector3(hInput, 0, fInput).normalized * speed * Time.deltaTime
+            + Vector3.up * verticalVelocity; // + gravity
+        
+        // Jump Impulse
+        
         cc.Move(moveInput);
     }
 }
